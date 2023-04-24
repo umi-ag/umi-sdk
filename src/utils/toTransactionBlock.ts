5ac7591b7058cae7f738b9a6b26df82d6a0367d5
imataka7 <t.imagawa@kakehashi.life>
2023-04-24 16:01:54 +0900

    Update toTxb

--- src/utils/toTransactionBlock.ts
+++ src/utils/toTransactionBlock.ts
@@ -3,8 +3,7 @@ import { TransactionBlock } from '@mysten/sui.js';
 import { findCoinByType } from '@umi-ag/sui-coin-list';
 import Decimal from 'decimal.js';
 import { err, ok } from 'neverthrow';
-import type { PriceQuote, SwapSettings } from 'umi-sdk';
-import { CoinAmount, is_x_to_y_ } from 'umi-sdk';
+import { match } from 'ts-pattern';
 import { packageBook } from '../addressList/testnet';
 import type { TradingRoute } from '../types';
 
@@ -19,7 +18,7 @@ export const make1hopSwapPayload = (
   const sourceCoinInfo = findCoinByType(sourceCoinType);
   const targetCoinInfo = findCoinByType(targetCoinType);
   if (!sourceCoinInfo || !targetCoinInfo) {
-    return err(new Error('coin not found'));
+    return err('coin not found');
   }
 
   const tradingUnit = route[0].chain[0].venues[0].venue;
@@ -28,11 +27,13 @@ export const make1hopSwapPayload = (
 
   const sourceCoinAmount = new Decimal(tradingUnit.amount_in)
     .mul(10 ** sourceCoinInfo.decimals)
-    .toFixed(0);
+    .round()
+    .toString();
   const minTargetCoinAmount = new Decimal(tradingUnit.min_amount_out)
-    .mul(10 ** targetCoinInfo.decimals)
     .mul(1 - slippageTolerance)
-    .toFixed(0);
+    .mul(10 ** targetCoinInfo.decimals)
+    .round()
+    .toString();
 
   const first_direction = tradingUnit.is_x_to_y ? 'x' : 'y';
   const first_package = tradingUnit.protocol_name;
@@ -53,97 +54,124 @@ export const make1hopSwapPayload = (
 };
 
 export const make2hopSwapPayload = (
-  quote: PriceQuote,
-  swapSettings: SwapSettings,
+  route: TradingRoute,
+  slippageTolerance: number,
   coins_s: PaginatedCoins['data'],
 ) => {
   if (coins_s.length < 1) return err('The length input coins is less than 1');
 
-  const CoinS = quote.fromCoin.coinInfo;
-  const CoinT = quote.toCoin.coinInfo;
-  const first_pool = quote.swapRoute1.pool.objectId;
-  const second_pool = quote.swapRoute2.pool.objectId;
+  const sourceCoinType = route[0].chain[0].source_coin;
+  const targetCoinType = route[0].chain[1].target_coin;
 
-  const first_is_x_to_y = is_x_to_y_(quote.swapRoute1);
-  const second_is_x_to_y = is_x_to_y_(quote.swapRoute2);
+  const sourceCoinInfo = findCoinByType(sourceCoinType);
+  const targetCoinInfo = findCoinByType(targetCoinType);
+  if (!sourceCoinInfo || !targetCoinInfo) {
+    return err('coin not found');
+  }
+
+  const firstTradingUnit = route[0].chain[0].venues[0].venue;
+  const secondTradingUnit = route[0].chain[1].venues[0].venue;
+
+  const first_venue = firstTradingUnit.venue_object_id;
+  const second_venue = secondTradingUnit.venue_object_id;
+
+  const first_is_x_to_y = firstTradingUnit.is_x_to_y;
+  const second_is_x_to_y = secondTradingUnit.is_x_to_y;
   const first_direction = first_is_x_to_y ? 'x' : 'y';
   const second_direction = second_is_x_to_y ? 'x' : 'y';
-  const first_package = quote.swapRoute1.pool.protocolName;
-  const second_package = quote.swapRoute2.pool.protocolName;
+  const first_package = firstTradingUnit.protocol_name;
+  const second_package = firstTradingUnit.protocol_name;
 
-  const sourceCoinAmount = quote.fromCoin;
-  const minTargetCoinAmount = new CoinAmount(
-    quote.toCoin.coinInfo,
-    quote.toCoin.amount.mul(swapSettings.slippageTolerance)
-  );
+  const sourceCoinAmount = new Decimal(firstTradingUnit.amount_in)
+    .mul(10 ** sourceCoinInfo.decimals)
+    .toFixed(0);
+  const minTargetCoinAmount = new Decimal(secondTradingUnit.min_amount_out)
+    .mul(10 ** targetCoinInfo.decimals)
+    .mul(1 - slippageTolerance)
+    .toFixed(0);
 
-  const CoinU = first_is_x_to_y
-    ? quote.swapRoute1.pool.pair.coinY.coinInfo
-    : quote.swapRoute1.pool.pair.coinX.coinInfo;
+  const coinUType = first_is_x_to_y
+    ? firstTradingUnit.target_coin
+    : firstTradingUnit.source_coin;
 
   const txb = new TransactionBlock();
   txb.moveCall({
     target: `${packageBook.sui_aggregator.packageObjectId}::${packageBook.sui_aggregator.modules.scripts}::two_hop_${first_direction}${second_direction}_${first_package}_${second_package}_script`,
-    typeArguments: [CoinS.type, CoinT.type, CoinU.type],
+    typeArguments: [sourceCoinType, targetCoinInfo.type, coinUType],
     arguments: [
-      txb.pure(first_pool),
-      txb.pure(second_pool),
+      txb.pure(first_venue),
+      txb.pure(second_venue),
       txb.makeMoveVec({ objects: coins_s.map(coin => txb.object(coin.coinObjectId)) }),
-      txb.pure(sourceCoinAmount.toU64),
-      txb.pure(minTargetCoinAmount.toU64),
+      txb.pure(sourceCoinAmount),
+      txb.pure(minTargetCoinAmount),
     ],
   });
   return ok(txb);
 };
 
 export const make3hopSwapPayload = (
-  quote: PriceQuote,
-  swapSettings: SwapSettings,
+  route: TradingRoute,
+  slippageTolerance: number,
   coins_s: PaginatedCoins['data'],
 ) => {
   if (coins_s.length < 1) return err('The length input coins is less than 1');
 
-  const CoinS = quote.fromCoin.coinInfo;
-  const CoinT = quote.toCoin.coinInfo;
-  const first_pool = quote.swapRoute1.pool.objectId;
-  const second_pool = quote.swapRoute2.pool.objectId;
-  const third_pool = quote.swapRoute3.pool.objectId;
+  const sourceCoinType = route[0].chain[0].source_coin;
+  const targetCoinType = route[0].chain[1].target_coin;
 
-  const first_is_x_to_y = is_x_to_y_(quote.swapRoute1);
-  const second_is_x_to_y = is_x_to_y_(quote.swapRoute2);
-  const third_is_x_to_y = is_x_to_y_(quote.swapRoute3);
+  const sourceCoinInfo = findCoinByType(sourceCoinType);
+  const targetCoinInfo = findCoinByType(targetCoinType);
+  if (!sourceCoinInfo || !targetCoinInfo) {
+    return err('coin not found');
+  }
+
+  const firstTradingUnit = route[0].chain[0].venues[0].venue;
+  const secondTradingUnit = route[0].chain[1].venues[0].venue;
+  const thirdTradingUnit = route[0].chain[2].venues[0].venue;
+
+  const first_venue = firstTradingUnit.venue_object_id;
+  const second_venue = secondTradingUnit.venue_object_id;
+  const third_venue = thirdTradingUnit.venue_object_id;
+
+  const first_is_x_to_y = firstTradingUnit.is_x_to_y;
+  const second_is_x_to_y = secondTradingUnit.is_x_to_y;
+  const third_is_x_to_y = thirdTradingUnit.is_x_to_y;
   const first_direction = first_is_x_to_y ? 'x' : 'y';
   const second_direction = second_is_x_to_y ? 'x' : 'y';
   const third_direction = third_is_x_to_y ? 'x' : 'y';
-  const first_package = quote.swapRoute1.pool.protocolName;
-  const second_package = quote.swapRoute2.pool.protocolName;
-  const third_package = quote.swapRoute3.pool.protocolName;
+  const first_package = firstTradingUnit.protocol_name;
+  const second_package = secondTradingUnit.protocol_name;
+  const third_package = thirdTradingUnit.protocol_name;
 
-  const sourceCoinAmount = quote.fromCoin;
-  const minTargetCoinAmount = new CoinAmount(
-    quote.toCoin.coinInfo,
-    quote.toCoin.amount.mul(swapSettings.slippageTolerance)
-  );
+  const sourceCoinAmount = new Decimal(firstTradingUnit.amount_in)
+    .mul(10 ** sourceCoinInfo.decimals)
+    .round()
+    .toString();
+  const minTargetCoinAmount = new Decimal(secondTradingUnit.min_amount_out)
+    .mul(1 - slippageTolerance)
+    .mul(10 ** targetCoinInfo.decimals)
+    .round()
+    .toString();
 
-  const CoinU = first_is_x_to_y
-    ? quote.swapRoute1.pool.pair.coinY.coinInfo
-    : quote.swapRoute1.pool.pair.coinX.coinInfo;
+  const coinUType = first_is_x_to_y
+    ? firstTradingUnit.target_coin
+    : firstTradingUnit.source_coin;
 
-  const CoinV = second_is_x_to_y
-    ? quote.swapRoute2.pool.pair.coinY.coinInfo
-    : quote.swapRoute2.pool.pair.coinX.coinInfo;
+  const coinVType = second_is_x_to_y
+    ? secondTradingUnit.target_coin
+    : secondTradingUnit.source_coin;
 
   const txb = new TransactionBlock();
   txb.moveCall({
     target: `${packageBook.sui_aggregator.packageObjectId}::${packageBook.sui_aggregator.modules.scripts}::three_hop_${first_direction}${second_direction}${third_direction}_${first_package}_${second_package}_${third_package}_script`,
-    typeArguments: [CoinS.type, CoinT.type, CoinU.type, CoinV.type],
+    typeArguments: [sourceCoinType, targetCoinType, coinUType, coinVType],
     arguments: [
-      txb.pure(first_pool),
-      txb.pure(second_pool),
-      txb.pure(third_pool),
+      txb.pure(first_venue),
+      txb.pure(second_venue),
+      txb.pure(third_venue),
       txb.makeMoveVec({ objects: coins_s.map(coin => txb.object(coin.coinObjectId)) }),
-      txb.pure(sourceCoinAmount.toU64),
-      txb.pure(minTargetCoinAmount.toU64),
+      txb.pure(sourceCoinAmount),
+      txb.pure(minTargetCoinAmount),
     ],
   });
 
@@ -151,40 +179,61 @@ export const make3hopSwapPayload = (
 };
 
 export const make2splitSwapPayload = (
-  quote: PriceQuote,
-  swapSettings: SwapSettings,
+  route: TradingRoute,
+  slippageTolerance: number,
   coins_s: PaginatedCoins['data'],
 ) => {
   if (coins_s.length < 1) return err('The length input coins is less than 1');
 
-  const CoinS = quote.fromCoin.coinInfo;
-  const CoinT = quote.toCoin.coinInfo;
-  const first_pool = quote.swapRoute1.pool.objectId;
-  const second_pool = quote.swapRoute2.pool.objectId;
+  const sourceCoinType = route[0].chain[0].source_coin;
+  const targetCoinType = route[0].chain[0].target_coin;
 
-  const first_is_x_to_y = is_x_to_y_(quote.swapRoute1);
-  const second_is_x_to_y = is_x_to_y_(quote.swapRoute2);
+  const sourceCoinInfo = findCoinByType(sourceCoinType);
+  const targetCoinInfo = findCoinByType(targetCoinType);
+  if (!sourceCoinInfo || !targetCoinInfo) {
+    return err('coin not found');
+  }
+
+  const firstTradingUnit = route[0].chain[0].venues[0].venue;
+  const secondTradingUnit = route[0].chain[0].venues[1].venue;
+  // to bps
+  const firstWeight = new Decimal(route[0].chain[0].venues[0].weight)
+    .mul(10 ** 4).round().toString();
+  const secondWeight = new Decimal(route[0].chain[0].venues[1].weight)
+    .mul(10 ** 4).round().toString();
+
+  const first_venue = firstTradingUnit.venue_object_id;
+  const second_venue = secondTradingUnit.venue_object_id;
+
+  const first_is_x_to_y = firstTradingUnit.is_x_to_y;
+  const second_is_x_to_y = secondTradingUnit.is_x_to_y;
   const first_direction = first_is_x_to_y ? 'x' : 'y';
   const second_direction = second_is_x_to_y ? 'x' : 'y';
-  const first_package = quote.swapRoute1.pool.protocolName;
-  const second_package = quote.swapRoute2.pool.protocolName;
+  const first_package = firstTradingUnit.protocol_name;
+  const second_package = secondTradingUnit.protocol_name;
 
-  const sourceCoinAmount = quote.fromCoin;
-  const minTargetCoinAmount = new CoinAmount(
-    quote.toCoin.coinInfo,
-    quote.toCoin.amount.mul(swapSettings.slippageTolerance)
-  );
+  const sourceCoinAmount = new Decimal(firstTradingUnit.amount_in)
+    .mul(10 ** sourceCoinInfo.decimals)
+    .round()
+    .toString();
+  const minTargetCoinAmount = new Decimal(secondTradingUnit.min_amount_out)
+    .mul(1 - slippageTolerance)
+    .mul(10 ** targetCoinInfo.decimals)
+    .round()
+    .toString();
 
   const txb = new TransactionBlock();
   txb.moveCall({
     target: `${packageBook.sui_aggregator.packageObjectId}::${packageBook.sui_aggregator.modules.scripts}::two_split_${first_direction}${second_direction}_${first_package}_${second_package}_script`,
-    typeArguments: [CoinS.type, CoinT.type],
+    typeArguments: [sourceCoinType, targetCoinType],
     arguments: [
-      txb.pure(first_pool),
-      txb.pure(second_pool),
+      txb.pure(first_venue),
+      txb.pure(second_venue),
       txb.makeMoveVec({ objects: coins_s.map(coin => txb.object(coin.coinObjectId)) }),
-      txb.pure(sourceCoinAmount.toU64),
-      txb.pure(minTargetCoinAmount.toU64),
+      txb.pure(firstWeight),
+      txb.pure(secondWeight),
+      txb.pure(sourceCoinAmount),
+      txb.pure(minTargetCoinAmount),
     ],
   });
 
@@ -192,47 +241,70 @@ export const make2splitSwapPayload = (
 };
 
 export const make3splitSwapPayload = (
-  quote: PriceQuote,
-  swapSettings: SwapSettings,
+  route: TradingRoute,
+  slippageTolerance: number,
   coins_s: PaginatedCoins['data'],
 ) => {
   if (coins_s.length < 1) return err('The length input coins is less than 1');
 
-  const CoinS = quote.fromCoin.coinInfo;
-  const CoinT = quote.toCoin.coinInfo;
-  const first_pool = quote.swapRoute1.pool.objectId;
-  const second_pool = quote.swapRoute2.pool.objectId;
-  const third_pool = quote.swapRoute3.pool.objectId;
+  const sourceCoinType = route[0].chain[0].source_coin;
+  const targetCoinType = route[0].chain[0].target_coin;
 
-  const first_is_x_to_y = is_x_to_y_(quote.swapRoute1);
-  const second_is_x_to_y = is_x_to_y_(quote.swapRoute2);
-  const third_is_x_to_y = is_x_to_y_(quote.swapRoute3);
+  const sourceCoinInfo = findCoinByType(sourceCoinType);
+  const targetCoinInfo = findCoinByType(targetCoinType);
+  if (!sourceCoinInfo || !targetCoinInfo) {
+    return err('coin not found');
+  }
+
+  const firstTradingUnit = route[0].chain[0].venues[0].venue;
+  const secondTradingUnit = route[0].chain[0].venues[1].venue;
+  const thirdTradingUnit = route[0].chain[0].venues[2].venue;
+  // to bps
+  const firstWeight = new Decimal(route[0].chain[0].venues[0].weight)
+    .mul(10 ** 3).round().toString();
+  const secondWeight = new Decimal(route[0].chain[0].venues[1].weight)
+    .mul(10 ** 3).round().toString();
+  const thirdWeight = new Decimal(route[0].chain[0].venues[2].weight)
+    .mul(10 ** 3).round().toString();
+
+  const first_venue = firstTradingUnit.venue_object_id;
+  const second_venue = secondTradingUnit.venue_object_id;
+  const third_venue = thirdTradingUnit.venue_object_id;
+
+  const first_is_x_to_y = firstTradingUnit.is_x_to_y;
+  const second_is_x_to_y = secondTradingUnit.is_x_to_y;
+  const third_is_x_to_y = thirdTradingUnit.is_x_to_y;
   const first_direction = first_is_x_to_y ? 'x' : 'y';
   const second_direction = second_is_x_to_y ? 'x' : 'y';
   const third_direction = third_is_x_to_y ? 'x' : 'y';
-  const first_package = quote.swapRoute1.pool.protocolName;
-  const second_package = quote.swapRoute2.pool.protocolName;
-  const third_package = quote.swapRoute3.pool.protocolName;
+  const first_package = firstTradingUnit.protocol_name;
+  const second_package = secondTradingUnit.protocol_name;
+  const third_package = thirdTradingUnit.protocol_name;
 
-  const sourceCoinAmount = quote.fromCoin;
-  const minTargetCoinAmount = new CoinAmount(
-    quote.toCoin.coinInfo,
-    quote.toCoin.amount.mul(swapSettings.slippageTolerance)
-  );
+  const sourceCoinAmount = new Decimal(firstTradingUnit.amount_in)
+    .mul(10 ** sourceCoinInfo.decimals)
+    .round()
+    .toString();
+  const minTargetCoinAmount = new Decimal(secondTradingUnit.min_amount_out)
+    .mul(1 - slippageTolerance)
+    .mul(10 ** targetCoinInfo.decimals)
+    .round()
+    .toString();
 
   const txb = new TransactionBlock();
   txb.moveCall({
     target: `${packageBook.sui_aggregator.packageObjectId}::${packageBook.sui_aggregator.modules.scripts}::three_split_${first_direction}${second_direction}${third_direction}_${first_package}_${second_package}_${third_package}_script`,
-    typeArguments: [CoinS.type, CoinT.type],
+    typeArguments: [sourceCoinType, targetCoinType],
     arguments: [
-      txb.pure(first_pool),
-      txb.pure(second_pool),
-      txb.pure(third_pool),
+      txb.pure(first_venue),
+      txb.pure(second_venue),
+      txb.pure(third_venue),
       txb.makeMoveVec({ objects: coins_s.map(coin => txb.object(coin.coinObjectId)) }),
-      txb.pure(quote.swapRoute1.part.mul(10 ** 3).round().toString()),
-      txb.pure(quote.swapRoute2.part.mul(10 ** 3).round().toString()),
-      txb.pure(sourceCoinAmount.toU64),
-      txb.pure(minTargetCoinAmount.toU64),
+      txb.pure(firstWeight),
+      txb.pure(secondWeight),
+      txb.pure(thirdWeight),
+      txb.pure(sourceCoinAmount),
+      txb.pure(minTargetCoinAmount),
     ],
   });
 
@@ -240,23 +312,17 @@ export const make3splitSwapPayload = (
 };
 
 export const makeSuiSwapPayload = (
-  quote: PriceQuote,
-  swapSettings: SwapSettings,
+  route: TradingRoute,
+  slippageTolerance: number,
   coins_s: PaginatedCoins['data'],
 ) => {
-  if (quote.swapType === 'split') {
-    if (quote.swapRoute3) {
-      return make3splitSwapPayload(quote, swapSettings, coins_s);
-    } else {
-      return make2splitSwapPayload(quote, swapSettings, coins_s);
-    }
-  } else {
-    if (quote.swapRoute3) {
-      return make3hopSwapPayload(quote, swapSettings, coins_s);
-    } else if (quote.swapRoute2) {
-      return make2hopSwapPayload(quote, swapSettings, coins_s);
-    } else {
-      return make1hopSwapPayload(quote, swapSettings, coins_s);
-    }
-  }
+  const nSplits = route[0].chain[0].venues.length;
+  const nHops = route[0].chain.length;
+
+  return match({ nSplits, nHops })
+    .with({ nSplits: 1, nHops: 1 }, () => make1hopSwapPayload(route, slippageTolerance, coins_s))
+    .with({ nSplits: 1, nHops: 2 }, () => make2hopSwapPayload(route, slippageTolerance, coins_s))
+    .with({ nSplits: 1, nHops: 3 }, () => make3hopSwapPayload(route, slippageTolerance, coins_s))
+    .with({ nSplits: 2, nHops: 1 }, () => make2splitSwapPayload(route, slippageTolerance, coins_s))
+    .with({ nSplits: 3, nHops: 1 }, () => make3splitSwapPayload(route, slippageTolerance, coins_s));
 };
