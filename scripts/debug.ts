a7649812eab6f09faf169fd02e9bf35d2967ae3d
imataka7 <t.imagawa@kakehashi.life>
2023-04-28 11:42:44 +0900

    Use private key

--- scripts/debug.ts
+++ scripts/debug.ts
@@ -1,4 +1,4 @@
-import { Connection, Ed25519Keypair, JsonRpcProvider, RawSigner } from '@mysten/sui.js';
+import { Connection, Ed25519Keypair, JsonRpcProvider, RawSigner, fromB64 } from '@mysten/sui.js';
 import fetch from 'cross-fetch';
 import { dev_fetchSplitQuotes } from '../src/api';
 import { makeTxbFromRoute } from '../src/utils/makeTransactionBlock';
@@ -12,23 +12,31 @@ const provider = new JsonRpcProvider(new Connection({
   fullnode: 'https://sui-api.rpcpool.com',
 }));
 
-const mnemonic = process.env.SUI_MNEMONIC as string;
-const keypair = Ed25519Keypair.deriveKeypair(mnemonic);
+const privatekey0x = (process.env.SUI_PRIVATE_KEY as string);
+const privatekey = privatekey0x.replace(/^0x/, ''); //slice used to remove the first 2 letter from the string and that's 0x
+const privateKeyBase64 = Buffer.from(privatekey, 'hex').toString('base64'); //convert hex to base64 string
+const keypair = Ed25519Keypair.fromSecretKey(fromB64(privateKeyBase64));
+// const mnemonic = process.env.SUI_MNEMONIC as string;
+// const keypair = Ed25519Keypair.deriveKeypair(mnemonic);
 // console.log(keypair.getPublicKey().toSuiAddress());
 const signer = new RawSigner(keypair, provider);
 const owner = keypair.getPublicKey().toSuiAddress();
+console.log(await signer.getAddress());
 
 // eslint-disable-next-line @typescript-eslint/no-extra-semi, no-extra-semi
 ;(async () => {
+  console.time('p');
   const sourceAmount = 3;
   const [quote] = await dev_fetchSplitQuotes({
     sourceCoin: devUSDC,
     targetCoin: devBTC,
     inputAmount: 3,
   });
+  console.timeLog('p', 'quote');
 
   const coins = (await provider.getCoins({ owner, coinType: devUSDC }))
     .data;
+  console.timeLog('p', 'coins');
   const r = makeTxbFromRoute(
     owner,
     sourceAmount,
@@ -36,6 +44,7 @@ const owner = keypair.getPublicKey().toSuiAddress();
     0,
     coins,
   );
+  console.timeLog('p', 'txb');
 
   if (r.isErr()) {
     console.error(r.error);
@@ -43,12 +52,13 @@ const owner = keypair.getPublicKey().toSuiAddress();
   }
 
   const txb = r.value;
-  console.log(JSON.stringify(JSON.parse(txb.serialize()), null, 2));
+  // console.log(JSON.stringify(JSON.parse(txb.serialize()), null, 2));
 
-  const result = await signer.dryRunTransactionBlock({
+  const dryRunResult = await signer.dryRunTransactionBlock({
     transactionBlock: txb,
   });
-  console.log(JSON.stringify(result, null, 2));
+  // console.log(JSON.stringify(dryRunResult, null, 2));
+  console.timeEnd('p');
 
   // const result = await signer.signAndExecuteTransactionBlock({
   //   transactionBlock: txb,
@@ -61,4 +71,5 @@ const owner = keypair.getPublicKey().toSuiAddress();
   //   },
   // });
   // console.log(JSON.stringify(result, null, 2));
+  // console.log(result.digest);
 })();
