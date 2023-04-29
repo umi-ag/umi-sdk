7fc126c8f5d5eadb89807bf5e3c647a9b61cd23b
imataka7 <t.imagawa@kakehashi.life>
2023-04-30 02:52:27 +0900

    Remove dev_fetchQuotes

--- typescript/sui-sdk/src/api/index.ts
+++ typescript/sui-sdk/src/api/index.ts
@@ -14,27 +14,7 @@ export const fetchQuotes = async ({
   const url = new URL(`${endpoint}/quote`);
   url.searchParams.append('source_coin', sourceCoin);
   url.searchParams.append('target_coin', targetCoin);
-  url.searchParams.append('input_amount', inputAmount.toString());
-  url.searchParams.append('max_hops', maxHops.toString());
-  url.searchParams.append('max_routes', maxRoutes.toString());
-
-  const response = await fetch(url.toString());
-  const result = await response.json();
-
-  return result;
-};
-
-export const dev_fetchSplitQuotes = async ({
-  sourceCoin,
-  targetCoin,
-  sourceCoinAmount: inputAmount,
-  maxHops = 2,
-  maxRoutes = 3,
-}: QuoteQuery): Promise<TradingRoutes> => {
-  const url = new URL(`${endpoint}/splits`);
-  url.searchParams.append('source_coin', sourceCoin);
-  url.searchParams.append('target_coin', targetCoin);
-  url.searchParams.append('input_amount', inputAmount.toString());
+  url.searchParams.append('source_amount', inputAmount.toString());
   url.searchParams.append('max_hops', maxHops.toString());
   url.searchParams.append('max_routes', maxRoutes.toString());
 
