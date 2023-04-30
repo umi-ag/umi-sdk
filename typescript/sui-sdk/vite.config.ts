042a0d89ae281cadaa752532a4ac30af80c127d9
fuyutarow <fuyutarow@gmail.com>
2023-04-30 16:31:36 +0900

    Fix vite.config

--- typescript/sui-sdk/vite.config.ts
+++ typescript/sui-sdk/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import path from "path";
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
@@ -10,31 +10,32 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      process: "process/browser",
-      stream: "rollup-plugin-node-polyfills/polyfills/stream",
-      events: "rollup-plugin-node-polyfills/polyfills/events",
+      process: 'process/browser',
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
     },
   },
   define: {
-    "process.env": process.env ?? {},
+    'process.env': process.env ?? {},
   },
   build: {
-    target: "esnext",
+    target: 'esnext',
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      formats: ["es", "umd"],
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'umd'],
+      name: '@umi-ag/sui-sdk',
       fileName: (format) => {
-        if (format === "es") {
-          return "index.mjs";
-        } else if (format === "umd") {
-          return "index.js";
+        if (format === 'es') {
+          return 'index.mjs';
+        } else if (format === 'umd') {
+          return 'index.js';
         }
       },
     },
   },
   optimizeDeps: {
     esbuildOptions: {
-      target: "esnext",
+      target: 'esnext',
     },
   },
 });
