d96f64d1a8d93662faa03965f360fc1e8c114be4
imataka7 <t.imagawa@kakehashi.life>
2023-05-07 22:38:57 +0900

    Update exapmles/simple.ts

--- typescript/sui-sdk/examples/simple.ts
+++ typescript/sui-sdk/examples/simple.ts
@@ -1,10 +1,14 @@
-import { Ed25519Keypair, JsonRpcProvider, RawSigner, fromB64, testnetConnection } from '@mysten/sui.js';
+import { Connection, Ed25519Keypair, JsonRpcProvider, RawSigner, fromB64 } from '@mysten/sui.js';
 import fetch from 'cross-fetch';
 import { fetchQuoteAndBuildTransactionBlockForUmiAgTrade } from '../src';
 
 globalThis.fetch = fetch;
 
-const provider = new JsonRpcProvider(testnetConnection);
+const provider = new JsonRpcProvider(
+  new Connection({
+    fullnode: 'https://fullnode.mainnet.sui.io',
+  }),
+);
 
 const keypair = () => {
   const privatekey0x = process.env.SUI_PRIVATE_KEY as string; // 0x.....
@@ -18,26 +22,32 @@ const signer = new RawSigner(keypair(), provider);
 const address = await signer.getAddress();
 console.log({ address });
 
-const devBTC = '0xda50fbb5eeb573e9825117b45564fd83abcdb487b5746f37a4a7c368f34a71ef::devnet_btc::DEVNET_BTC';
-const devUSDC = '0xda50fbb5eeb573e9825117b45564fd83abcdb487b5746f37a4a7c368f34a71ef::devnet_usdc::DEVNET_USDC';
+const SUI = '0x2::sui::SUI';
+// const WETHw = '0xaf8cd5edc19c4512f4259f0bee101a40d41ebed738ade5874359610ef8eeced5::coin::COIN';
+// const USDTw = '0xc060006111016b8a020ad5b33834984a437aaa7d3c74c18e09a95d48aceab08c::coin::COIN';
+const USDCw = '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN';
 
 (async () => {
   const txb = await fetchQuoteAndBuildTransactionBlockForUmiAgTrade({
     provider,
     accountAddress: address,
-    sourceCoinType: devBTC,
-    targetCoinType: devUSDC,
+    sourceCoinType: SUI,
+    targetCoinType: USDCw,
     sourceAmount: 1000n,
     slippageTolerance: 0.01, // 1%
   });
 
-  const result = await signer.signAndExecuteTransactionBlock({
+  console.log(JSON.stringify(JSON.parse(txb.serialize()), null, 2));
+  const dryRunResult = await signer.dryRunTransactionBlock({
     transactionBlock: txb,
-    options: {
-      showBalanceChanges: true,
-    },
   });
-
-  console.log(result.balanceChanges);
-  console.log(result.digest);
+  console.log(JSON.stringify(dryRunResult, null, 2));
+  // const result = await signer.signAndExecuteTransactionBlock({
+  //   transactionBlock: txb,
+  //   options: {
+  //     showBalanceChanges: true,
+  //   },
+  // });
+  // console.log(result.balanceChanges);
+  // console.log(result.digest);
 })();
