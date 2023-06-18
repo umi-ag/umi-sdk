026d26e1ebe2ecad4b4550ff301165a9bd47f67a
imataka7 <t.imagawa@kakehashi.life>
2023-06-18 23:17:35 +0900

    Add files

---
+++ typescript/aptos-sdk/src/config/index.ts
@@ -0,0 +1,8 @@
+import type { VenueName } from '../types';
+
+export const UMIAG_PACKAGE_ID = '0xb8261603668830ae4269b45abc6bae451f7f1b5d220de59a39b1bf29217261c2';
+
+export const DEFAULT_ENDPOINT = 'https://sui-beta.fly.dev';
+
+export const DEFAULT_ALLOW_LIST_VENUE_NAME: VenueName[] = [
+];
---
+++ typescript/aptos-sdk/src/types/index.ts
@@ -0,0 +1,74 @@
+export type VenueName = 'animeswap'
+  | 'pontem';
+
+export type Venue = {
+  name: VenueName;
+  object_id: string;
+  object_type: string;
+  source_coin: string;
+  target_coin: string;
+  // f64
+  source_amount: number;
+  // f64
+  target_amount: number;
+  source_fee: number;
+  target_fee: number;
+  is_x_to_y: boolean;
+  function: `${string}::${string}::${string}`;
+};
+
+export type WeightedVenue = {
+  venue: Venue;
+  // f64
+  weight: number;
+};
+
+export type Step = {
+  source_coin: string;
+  target_coin: string;
+  source_amount: number;
+  target_amount: number;
+  venues: WeightedVenue[];
+};
+
+export type Hop = {
+  source_coin: string;
+  target_coin: string;
+  source_amount: number;
+  target_amount: number;
+  steps: Step[];
+};
+
+export type WeightedHop = {
+  path: Hop;
+  // f64
+  weight: number;
+};
+
+export type TradingRoute = {
+  source_coin: string;
+  target_coin: string;
+  source_amount: number;
+  target_amount: number;
+  paths: WeightedHop[];
+};
+
+export type TradingRoutes = TradingRoute[];
+
+export type VenueInfo = {
+  venueObjectId: string,
+  coinTypeX: string,
+  coinTypeY: string,
+};
+
+export type VenueBook = Record<string, Record<string, VenueInfo>>;
+
+export type QuoteQuery = {
+  sourceCoin: string,
+  targetCoin: string,
+  sourceAmount: string | number,
+  // maxHops?: number,
+  // maxRoutes?: number,
+  endpoint?: `https://${string}`,
+  venueAllowList?: VenueName[],
+};
