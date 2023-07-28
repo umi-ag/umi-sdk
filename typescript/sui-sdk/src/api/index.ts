71106a82d655e2c664359c18675623ae5367357c
imataka7 <t.imagawa@kakehashi.life>
2023-07-29 01:40:13 +0900

    Add api

--- typescript/sui-sdk/src/api/index.ts
+++ typescript/sui-sdk/src/api/index.ts
@@ -25,6 +25,29 @@ export const fetchQuoteFromUmi = async ({
   return result;
 };
 
+export const fetchQuoteX = async ({
+  sourceCoin,
+  targetCoin,
+  sourceAmount,
+  // maxHops = 2,
+  // maxRoutes = 1,
+  endpoint = DEFAULT_ENDPOINT,
+  venueAllowList = DEFAULT_ALLOW_LIST_VENUE_NAME,
+}: QuoteQuery): Promise<TradingRoutes> => {
+  const url = new URL(`${endpoint}/quote_x`);
+  url.searchParams.append('source_coin', sourceCoin);
+  url.searchParams.append('target_coin', targetCoin);
+  url.searchParams.append('source_amount', sourceAmount.toString());
+  url.searchParams.append('venue_allow_list', venueAllowList.join(','));
+
+  // console.log(url.toString());
+
+  const response = await fetch(url.toString());
+  const result = await response.json();
+
+  return result;
+};
+
 export const _fetchQuotesFromUmi = async ({
   sourceCoin,
   targetCoin,
